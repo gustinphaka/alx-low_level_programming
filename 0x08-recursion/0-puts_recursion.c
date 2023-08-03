5a52a7946eb474d9ab9f4d22a3d07bd97e35a6ee
gustinphaka <gustinphaka@gmail.com>
2023-08-03 07:56:48 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
  */
 
 void _puts_recursion(char *s)
@@ -13,5 +13,7 @@ if (*s)
 _putchar(*s);
 _puts_recursion(s + 1);
 }
+
+else
 _putchar('\n');
 }
