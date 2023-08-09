ea9d81e5c1013e72726fe3ee2c97cad7402b7f97
gustinphaka <gustinphaka@gmail.com>
2023-08-08 19:17:59 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,7 +10,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-char *concat;
+char *conct;
 int i, ci;
 
 if (s1 == NULL)
@@ -23,21 +23,21 @@ while (s1[i] != '\0')
 i++;
 while (s2[i] != '\0')
 ci++;
-concat = malloc(sizeof(char) * (i + ci + 1));
+conct = malloc(sizeof(char) * (i + ci + 1));
 
-if (concat == NULL)
+if (conct == NULL)
 return (NULL);
 i = ci = 0;
 while (s1[i] != '\0')
 {
-concat[i] = s1[i];
+conct[i] = s1[i];
 i++;
 }
 while (s2[ci] != '\0')
 {
-concat[i] = s2[ci];
+conct[i] = s2[ci];
 i++, ci++;
 }
-concat[i] = '\0';
+conct[i] = '\0';
 return (concat);
 }
