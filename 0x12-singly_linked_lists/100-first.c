47128d3decf7a46f74d4383c0658d21f5964bb3c
gustinphaka <gustinphaka@gmail.com>
2023-08-24 03:45:08 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,5 +10,5 @@ void first(void) __attribute__((constructor));
 void first(void)
 {
 printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house uponmy back!\n");
+printf("I bore my house upon my back!\n");
 }
