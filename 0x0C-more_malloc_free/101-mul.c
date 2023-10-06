224679e614b22c9306dd51e0875dc5b787735fb5
gustinphaka <gustinphaka@gmail.com>
2023-10-05 19:14:28 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -76,10 +76,6 @@ if (!result)
 return (1);
 for (i = 0; i <= len1 + len2; i++)
 result[i] = 0;
-if (!result)
-return (1);
-for (i = 0; i <= len1 + len2; i++)
-result[i] = 0;
 for (len1 = len1 - 1; len1 >= 0; len1--)
 
 {
@@ -91,7 +87,7 @@ for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 {
 
 digit2 = s2[len2] - '0';
-carry += result[len1 + len2 + 1] + (digit1 *digit2);
+carry += result[len1 + len2 + 1] + (digit1 * digit2);
 result[len1 + len2 + 1] = carry % 10;
 carry /= 10;
 
@@ -108,12 +104,12 @@ for (i = 0; i < len - 1; i++)
 
 if (result[i])
 a = 1;
-if (!a)
+if (a)
 _putchar(result[i] + '0');
 
 }
 
-if (!a)
+if (a)
 _putchar('0');
 _putchar('\n');
 free(result);
