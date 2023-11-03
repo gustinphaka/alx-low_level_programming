056b5fb56db52d3512e5165639acc0d3f90b8d45
gustinphaka <gustinphaka@gmail.com>
2023-11-02 21:18:16 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,3 +23,19 @@ return (i);
  *
  * Return; 1 on success 0 on failure
  */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t bytes = 0, len = _strlen(text_content);
+
+if (!filename)
+return (-1);
+fd = open(filename, O_WRONGLY | O_APPEND);
+if (fd == -1)
+return (-1);
+if (len)
+bytes = write(fd, text_content, len);
+close(fd);
+return (bytes == len ? 1 : -1);
+}
